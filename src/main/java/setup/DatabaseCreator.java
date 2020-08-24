7464fae5bfc0e031673280fd384489b6f234e55c
Janne Saari <jannesaari24@gmail.com>
2020-08-24 09:41:13 +0300

    Add files for SQL Statements

--- src/main/java/setup/DatabaseCreator.java
+++ src/main/java/setup/DatabaseCreator.java
@@ -1,7 +1,14 @@
 package setup;
 
 import java.io.Console;
+import java.io.File;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.io.IOException;
 import java.sql.*;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 public class DatabaseCreator {
 
@@ -27,14 +34,14 @@ public class DatabaseCreator {
      * more info about postgresql and ssl 
      static final String url = "jdbc:postgresql://localhost/postgres?ssl=true";
      */
-    static final String postgresUrl = "jdbc:postgresql://localhost/postgres?allowMultiQueries=true";
+    static final String hostUrl = "jdbc:postgresql://localhost/";
     static final String musicDBUrl = "jdbc:postgresql://localhost/musicdb?allowMultiQueries=true";
     static String user = "postgres";
     static String password = "";
     static boolean dbIsPostgress = true;
 
     public static Connection connect() throws SQLException {
-        return DriverManager.getConnection(postgresUrl, user, password);
+        return DriverManager.getConnection(hostUrl, user, password);
     }
     
     private static void askAuthInfo() {
@@ -51,37 +58,37 @@ public class DatabaseCreator {
         }
     }
 
+    //TODO move to helper class later when its created
+    public static String loadResourceToString(final String path){
+        final InputStream stream =
+            Thread
+                .currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream(path);
+        try{
+            return IOUtils.toString(stream, Charset.defaultCharset());
+        } catch(final IOException e){
+            throw new IllegalStateException(e);
+        } finally{
+            //IOUtils.closeQuietly(stream);
+        }
+    }
+
     static int SQLAction(String sql) {
         Connection conn = null;
+        //TODO read about PreparedStatement
+        //PreparedStatement statement = null;
         Statement statement = null;
 
         try {
             conn = DriverManager.getConnection(musicDBUrl, user, password);
+            //statement = conn.prepareStatement(sql);
             statement = conn.createStatement();
-            statement.execute(sql);
+            statement.executeUpdate(sql);
 
             System.out.println("Action executed successfully...");
         } catch (SQLException e) {
             System.out.println("Action execution failed!: " + System.lineSeparator() + "\"" + sql + "\"");
-            e.printStackTrace();
-            return -1;
-        }
-
-        return 0;
-    }
-
-    static int DropDatabase() {
-        Connection conn = null;
-        Statement statement = null;
-
-        try {
-            conn = DriverManager.getConnection(postgresUrl, user, password);
-            statement = conn.createStatement();
-            statement.execute(ClearDatabase);
-
-            System.out.println("Database dropped successfully...");
-        } catch (SQLException e) {
-            System.out.println("Database dropping failed!: " + System.lineSeparator() + "\"" + ClearDatabase + "\"");
             e.printStackTrace();
             return -1;
         }
@@ -97,30 +104,33 @@ public class DatabaseCreator {
 
         try {
             System.out.println("Connecting to server...");
-            conn = DriverManager.getConnection(postgresUrl, user, password);
+            conn = DriverManager.getConnection(hostUrl, user, password);
 
             //TODO delete this
             //or ask user if they have premade database or something
-            DropDatabase();
+            //DropDatabase();
 
             System.out.println("Creating database...");
             statement = conn.createStatement();
 
-            statement.executeUpdate(Database);
+            File dbCreationFile = new File("src/main/resources/SQL/DatabaseCreation");
+            String dbString = FileUtils.readFileToString(dbCreationFile, Charset.defaultCharset());
+            //String dbString = loadResourceToString(file.getAbsolutePath());
+            statement.execute(dbString);
             System.out.println("Database created successfully...");
 
             //If database is postgresql, get uuid_ossp extension
-            if(dbIsPostgress)
+            if(dbIsPostgress){
+                File SQLExtensionsFile = new File("src/main/resources/SQL/Extensions");
+                String Extensions = FileUtils.readFileToString(SQLExtensionsFile, Charset.defaultCharset());
                 SQLAction(Extensions);
+            }
 
-            SQLAction(ArtistTable);
-            SQLAction(AlbumTable);
-            SQLAction(PlaylistTable);
-            SQLAction(PlaylistSongsTable);
-            SQLAction(UserTable);
-            SQLAction(PasswordTable);
-            SQLAction(FileTable);
-            SQLAction(SongTable);
+            File TableCreationFile = new File("src/main/resources/SQL/TableCreation");
+            String TableString = FileUtils.readFileToString(TableCreationFile, Charset.defaultCharset());
+            //String dbString = loadResourceToString(file.getAbsolutePath());
+            SQLAction(TableString);
+            System.out.println("Tables created successfully...");
             
         } catch (SQLException SQLe) {
             // Handle error for JDBC
@@ -132,109 +142,112 @@ public class DatabaseCreator {
             // finally block used to close resources
             try {
                 if (statement != null)
-                    statement.close();
+                statement.close();
             } catch (SQLException SQLe2) {
                 // TODO: handle exception
             }
             try {
                 if (conn != null)
                     conn.close();
-            } catch (SQLException SQLe) {
-                SQLe.printStackTrace();
+                } catch (SQLException SQLe) {
+                    SQLe.printStackTrace();
+                }
             }
+            System.out.println("Goodbye!");
         }
-        System.out.println("Goodbye!");
-    }
-
-    static String Database = "CREATE DATABASE musicdb";
-
     
-    static String ArtistTable = 
-    """
-    CREATE TABLE artist(
-        id UUID not NULL, 
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String Database = "CREATE DATABASE musicdb";
     
-    static String AlbumTable = 
-    """
-    CREATE TABLE album (
-        id UUID not NULL,
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String SongTable = 
+    // """
+    // CREATE TABLE song (
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     AlbumID UUID,
+    //     ArtistID UUID,
+    //     name VARCHAR(255), 
+    //     PRIMARY KEY ( id ))
+    // """;
     
-    static String PlaylistTable = 
-    """
-    CREATE TABLE playlist(
-        id UUID not NULL,
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String ArtistTable = 
+    // """
+    // CREATE TABLE artist(
+    //     id UUID DEFAULT gen_random_uuid(), 
+    //     name VARCHAR(255),
+    //     PRIMARY KEY ( id ))
+    // """; 
     
-    static String PlaylistSongsTable = 
-    """
-    CREATE TABLE playlistsongs(
-        id UUID not NULL,
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String AlbumTable = 
+    // """
+    // CREATE TABLE album (
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     name VARCHAR(255),
+    //     ArtistID UUID,
+    //     PRIMARY KEY ( id ))
+    // """; 
     
-    static String UserTable = 
-    """
-    CREATE TABLE user_account(
-        id UUID not NULL,
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String PlaylistTable = 
+    // """
+    // CREATE TABLE playlist(
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     name VARCHAR(255),
+    //     CreatorID UUID,
+    //     PRIMARY KEY ( id ))
+    // """; 
     
-    static String PasswordTable = 
-    """
-    CREATE TABLE password(
-        id UUID not NULL,
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String PlaylistSongsTable = 
+    // """
+    // CREATE TABLE playlistsongs(
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     playlistID UUID,
+    //     songID UUID,
+    //     orderNumber SMALLINT,
+    //     PRIMARY KEY ( id ))
+    // """; 
     
-    static String FileTable = 
-    """
-    CREATE TABLE file(
-        id UUID not NULL,
-        name VARCHAR(255),
-        PRIMARY KEY ( id ))
-    """; 
+    // static String UserTable = 
+    // """
+    // CREATE TABLE user_account(
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     name VARCHAR(255),
+    //     PRIMARY KEY ( id ))
+    // """; 
+    
+    // //TODO store password as hash
+    // static String PasswordTable = 
+    // """
+    // CREATE TABLE password(
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     pw VARCHAR(255),
+    //     userID UUID,
+    //     PRIMARY KEY ( id ))
+    // """; 
+    
+    // static String FileTable = 
+    // """
+    // CREATE TABLE file(
+    //     id UUID DEFAULT gen_random_uuid(),
+    //     filepath VARCHAR(255),
+    //     PRIMARY KEY ( id ))
+    // """; 
 
-    static String SongTable = 
-    """
-    CREATE TABLE song (
-        id UUID DEFAULT gen_random_uuid(),
-        AlbumID UUID,
-        ArtistID UUID,
-        name VARCHAR(255), 
-        PRIMARY KEY ( id ),
-        CONSTRAINT fk_ArtistID
-            FOREIGN KEY(ArtistID)
-                REFERENCES Artist(id))
-    """;
 
-    static String Extensions = 
-    """
-    CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-    """;
+    // static String Extensions = 
+    // """
+    // CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+    // """;
 
-    //TODO delete later
-    //Used to clear database while testing
-    static String ClearDatabase = 
-    """
-    SELECT *
-    FROM pg_stat_activity
-    WHERE datname = 'musicdb';
+    // //TODO delete later
+    // //Used to clear database while testing
+    // static String ClearDatabase = 
+    // """
+    // SELECT *
+    // FROM pg_stat_activity
+    // WHERE datname = 'musicdb';
 
-    SELECT pg_terminate_backend (pid)
-    FROM pg_stat_activity
-    WHERE pg_stat_activity.datname = 'musicdb';
+    // SELECT pg_terminate_backend (pid)
+    // FROM pg_stat_activity
+    // WHERE pg_stat_activity.datname = 'musicdb';
 
-    DROP DATABASE IF EXISTS musicdb
-    """;
+    // DROP DATABASE IF EXISTS musicdb
+    // """;
 }
